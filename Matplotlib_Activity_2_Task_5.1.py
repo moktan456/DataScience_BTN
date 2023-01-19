a2e3fc0835ad993767730b64de2f92586ef69437
Nima Dorji Moktan <moktannima2015@gmail.com>
2023-01-19 11:39:31 +0600

    Activity 2 - Task 5.1 and 7 added

---
+++ Matplotlib_Activity_2_Task_5.1.py
@@ -0,0 +1,22 @@
+# Activity 2 - Task 5
+# Using fill_between function
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = ['English','Dzongkha','Math','Science','History','Geography']
+std1 = np.array([67,75,83,66,55,82])
+std2 = np.array([75,66,34,89,59,94])
+
+fig, ax = plt.subplots()
+ax.plot(x,std1, label = 'std_1')
+ax.plot(x,std2, label = 'std_2')
+
+ax.fill_between(x,std1,std2, where=(std1>std2),interpolate=True, color='green')
+
+ax.set_xlabel('Subject')
+ax.set_ylabel('Score')
+ax.legend()
+
+plt.show()
+
+
---
+++ Matplotlib_Activity_2_Task_7.py
@@ -0,0 +1,15 @@
+# Activity 2 - Task 7
+# Using histogram
+import matplotlib.pyplot as plt
+
+# Exam scores
+scores = [65, 72, 80, 59, 93, 80, 75, 68, 55, 85, 60, 84, 70, 76, 86]
+
+# Create histogram
+plt.hist(scores, bins=10)
+plt.xlabel('Score')
+plt.ylabel('Frequency')
+plt.title('Exam Scores Histogram')
+
+# Show histogram
+plt.show()
